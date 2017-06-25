e11b889d401d263cfecc7572d7357f68087017cd
Yaron Karasik <yaronkarasik@gmail.com>
2017-06-25 19:55:50 +0300

    initial commit

---
+++ AudioKitRouting/AudioEngine.swift
@@ -0,0 +1,138 @@
+//
+//  AudioEngine.swift
+//  AudioKitRouting
+//
+//  Created by Yaron Karasik on 6/25/17.
+//  Copyright © 2017 karasik. All rights reserved.
+//
+
+import Foundation
+import AudioKit
+
+protocol AudioEngineDelegate {
+    func playbackStateDidChange(playing: Bool)
+    func headphonesStateDidChange(connected: Bool)
+}
+
+class AudioEngine {
+    var delegate: AudioEngineDelegate?
+    
+    var mixer: AKMixer?
+    var player: AKAudioPlayer?
+    
+    var isPlaying = false
+    var shouldBePlaying = false
+    
+    init() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(audioRouteChangeListener(notification:)),
+            name: NSNotification.Name.AVAudioSessionRouteChange,
+            object: nil)
+        
+        setupAudioKit()
+    }
+    
+    public func setupAudioKit() {
+        AudioKit.stop()
+        
+        AKSettings.defaultToSpeaker = true
+        AKSettings.disableRouteChangeHandling = true
+        
+        do {
+            try AKSettings.setSession(category: .playAndRecord, with: [.defaultToSpeaker, .allowBluetooth, .allowBluetoothA2DP])
+        } catch {
+            print("Errored setting category.")
+        }
+        
+        do {
+            guard let filePath = Bundle.main.path(forResource: "guitar", ofType: "wav") else { return }
+            guard let fileUrl = URL(string: filePath) else { return }
+            let audioFile = try AKAudioFile(forReading: fileUrl)
+            player = try AKAudioPlayer(file: audioFile)
+        } catch let error {
+            fatalError("AudioKit: Could not load audio file. error: \(error).")
+        }
+        
+        mixer = AKMixer()
+        if let player = player, let mixer = mixer {
+            mixer.connect(player)
+            AudioKit.output = mixer
+        }
+        
+        AudioKit.start()
+        
+        if shouldBePlaying {
+            play()
+        }
+    }
+    
+    public func togglePlaying() {
+        isPlaying ? stop() : play()
+        shouldBePlaying = isPlaying
+    }
+    
+    private func stop() {
+        guard let player = player else { return }
+        guard isPlaying else { return }
+        isPlaying = false
+        player.stop()
+        if let delegate = delegate {
+            delegate.playbackStateDidChange(playing: false)
+        }
+    }
+    
+    private func play() {
+        guard let player = player else { return }
+        guard !isPlaying else { return }
+        
+        player.completionHandler = {
+            self.isPlaying = false
+            if player.currentTime == player.endTime {
+                self.shouldBePlaying = false
+            }
+            if let delegate = self.delegate {
+                DispatchQueue.main.async {
+                    delegate.playbackStateDidChange(playing: false)
+                }                
+            }
+        }
+        self.isPlaying = true
+        if let delegate = delegate {
+            delegate.playbackStateDidChange(playing: true)
+        }
+        player.play()
+    }
+
+    @objc private func audioRouteChangeListener(notification: Notification) {
+        let audioRouteChangeReason = notification.userInfo![AVAudioSessionRouteChangeReasonKey] as! UInt
+        
+        let headphonesConnected = areHeadphonesConnected
+        switch (audioRouteChangeReason, headphonesConnected) {
+        case (AVAudioSessionRouteChangeReason.newDeviceAvailable.rawValue, true):
+            setupAudioKit()
+            if let delegate = delegate {
+                delegate.headphonesStateDidChange(connected: headphonesConnected)
+            }
+        case (AVAudioSessionRouteChangeReason.oldDeviceUnavailable.rawValue, false):
+            setupAudioKit()
+            if let delegate = delegate {
+                delegate.headphonesStateDidChange(connected: headphonesConnected)
+            }
+        default:
+            break
+        }
+    }
+    
+    var areHeadphonesConnected: Bool {
+        let currentRoute = AVAudioSession.sharedInstance().currentRoute
+        for description in currentRoute.outputs {
+            if description.portType == AVAudioSessionPortHeadphones {
+                return true
+            }
+        }
+        return false
+    }
+
+}
+
--- AudioKitRouting/ViewController.swift
+++ AudioKitRouting/ViewController.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
+import AudioKit
 
 class ViewController: UIViewController {
+    var audioEngine = AudioEngine()
 
+    lazy var playPauseButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = UIColor.green
+        button.layer.cornerRadius = 50.0
+        button.layer.borderWidth = 10.0
+        button.layer.borderColor = UIColor.white.cgColor
+        button.addTarget(self, action: #selector(playButtonWasTapped), for: UIControlEvents.touchUpInside)
+        return button
+    }()
+    
+    lazy var headphonesLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = UIColor.white
+        label.font = UIFont.systemFont(ofSize: 20.0)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        audioEngine.delegate = self
+        view.backgroundColor = UIColor(white: 0.5, alpha: 1.0)
+        setupPlayPauseButton()
+        setupHeadphonesLabel()
+        
+        updateHeadphonesLabel(connected: audioEngine.areHeadphonesConnected)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setupPlayPauseButton() {
+        playPauseButton.frame = CGRect(x: 0.0, y: 0.0, width: 100.0, height: 100.0)
+        playPauseButton.center = view.center
+        view.addSubview(playPauseButton)
     }
+    
+    func setupHeadphonesLabel () {
+        headphonesLabel.frame = CGRect(x: 0.0, y: view.bounds.size.height - 50.0, width: view.bounds.size.width, height: 20.0)
+        view.addSubview(headphonesLabel)
+    }
+    
+    func updateHeadphonesLabel(connected: Bool) {
+        headphonesLabel.text = connected ? "HEADPHONES CONNECTED" : "HEADPHONES DISCONNECTED"
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    func playButtonWasTapped() {
+        audioEngine.togglePlaying()        
+    }
+}
 
-
+extension ViewController: AudioEngineDelegate {
+    func playbackStateDidChange(playing: Bool) {
+        playPauseButton.backgroundColor = playing ? UIColor.red : UIColor.green
+    }
+    
+    func headphonesStateDidChange(connected: Bool) {
+        updateHeadphonesLabel(connected: connected)
+    }
 }
 
